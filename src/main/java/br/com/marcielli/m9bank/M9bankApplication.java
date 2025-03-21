1e8ddee4a7655e89fa6e9e4398db7dd0f01fc1db
Marcielli Oliveira <marciellileticiaol@gmail.com>
2025-03-21 10:18:12 -0300

    Preparação do Projeto

---
+++ src/main/java/br/com/marcielli/m9bank/M9bankApplication.java
@@ -0,0 +1,12 @@
+package br.com.marcielli.m9bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M9bankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M9bankApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/marcielli/m9bank/M9bankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcielli.m9bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M9bankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
